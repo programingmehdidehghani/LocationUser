0852ef826bf6c57de5eac7b80f14f28261215acb
mehdi <dehghani_m@rocketmail.com>
2023-04-10 13:27:51 +0430

    make db

--- app/src/main/java/com/example/locationservice/db/LocationDatabase.kt
+++ app/src/main/java/com/example/locationservice/db/LocationDatabase.kt
@@ -9,5 +9,5 @@ import androidx.room.RoomDatabase
     version = 1
 )
 abstract class LocationDatabase : RoomDatabase() {
-    abstract fun getRunDao() : LocationDAO
+    abstract fun getLocationDAO() : LocationDAO
 }
--- app/src/main/java/com/example/locationservice/util/Constants.kt
+++ app/src/main/java/com/example/locationservice/util/Constants.kt
@@ -8,5 +8,6 @@ object Constants {
     const val NOTIFICATION_CHANNEL_NAME = "tracking"
     const val NOTIFICATION_ID = 1
     const val ACTION_SHOW_TRACKING_FRAGMENT = "ACTION_SHOW_TRACKING_FRAGMENT"
+    const val LOCATION_DATABASE_NAME = "location_db"
 
 }
